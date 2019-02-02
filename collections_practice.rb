e1dfbd4888bd15a161ddd7bb956f45b0e23cbb9d
Kyle Spencer <spencerk2@vcu.edu>
2019-02-02 19:15:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? {|x| x[0] == "r"}
+end
+
+def contain_a(array)
+  array.select {|x| x.include? ("a")}
+end
+
+def first_wa(array)
+  new_array = array.join(",")
+  search_array = new_array.split(",")
+  search_array.find {|x| x.include? ("wa")}
+end
